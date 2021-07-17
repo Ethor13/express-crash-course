0eafa75bc842b803e279f34d42ddcd04c39fa29d
ethor13 <ethaniorlowsky@gmail.com>
2021-07-16 21:57:07 -0700

    apply middleware

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
+const logger = require("./middleware/logger");
 
 const app = express();
 
+// Init middleware
+// app.use(logger);
+
+// Gets all Members
+app.get("/api/members", (req, res) => res.json(members));
+
+// Get Single Member
+app.get("/api/members/:id", (req, res) => {
+	const found = members.some(
+		(member) => member.id === parseInt(req.params.id)
+	);
+	if (found) {
+		res.json(
+			members.filter((member) => member.id === parseInt(req.params.id))
+		);
+	} else {
+		res.status(400).json({
+			msg: "Member not found",
+		});
+	}
+});
+
+// Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
